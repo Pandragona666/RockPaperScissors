17455040d0d1af0c5dd37ffe797e80eccf844eb1
Pandragona <asiuspospolitus@gmail.com>
2019-04-21 19:52:47 +0200

    addint test for checkingBattleResultsmethod

--- src/test/java/pl/kordas/portfolio/GameServiceTest.java
+++ src/test/java/pl/kordas/portfolio/GameServiceTest.java
@@ -2,9 +2,8 @@ package pl.kordas.portfolio;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.w3c.dom.ranges.Range;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.*;
 
 public class GameServiceTest {
@@ -16,9 +15,28 @@ public class GameServiceTest {
         gameService = new GameService();
     }
 
+
+    //("1 - Rock ; 2 - Paper ; 3 - Scissors");
+    /*@ParameterizedTest(name = "expecting \"{2}\" when given {0} and {1}")
+    @CsvSource({
+            // given: 1 and 1, expected "0"
+            "1, 1, 0",
+            // given: 1 and 1, expected "0"
+            "2, 2, 0",
+            // given: 1 and 1, expected "0"
+            "3, 3, 0",
+    })
+    public void checkingBattleInCaseOfTheSameWeapons(int input, int input2, int expected) throws Exception {
+        //when
+        int result = gameService.checkingBattleResults(input,input2);
+
+        //then
+        assertThat(result).isEqualTo(expected);
+    }*/
+
     //("1 - Rock ; 2 - Paper ; 3 - Scissors");
     @Test
-    public void checkingBattleRockAgainstScissors() throws Exception {
+    public void checkingBattleRockAgainstPaper() throws Exception {
         //given
         final int playerInput = 1;
         final int opponentInput = 2;
@@ -31,6 +49,55 @@ public class GameServiceTest {
         assertThat(gameService.getOpponentScore()).isEqualTo(1);
         assertThat(gameService.getPlayerScore()).isEqualTo(0);
     }
+
+
+    //("1 - Rock ; 2 - Paper ; 3 - Scissors");
+    @Test
+    public void checkingBattlePaperAgainstRock() throws Exception {
+        //given
+        final int playerInput = 2;
+        final int opponentInput = 1;
+
+        //when
+        gameService.checkingBattleResults(playerInput,opponentInput);
+
+
+        //then
+        assertThat(gameService.getOpponentScore()).isEqualTo(0);
+        assertThat(gameService.getPlayerScore()).isEqualTo(1);
+    }
+
+    @Test
+    public void checkingBattlePaperAgainstScissors() throws Exception {
+        //given
+        final int playerInput = 2;
+        final int opponentInput = 3;
+
+        //when
+        gameService.checkingBattleResults(playerInput,opponentInput);
+
+
+        //then
+        assertThat(gameService.getOpponentScore()).isEqualTo(1);
+        assertThat(gameService.getPlayerScore()).isEqualTo(0);
+    }
+
+    @Test
+    public void checkingBattleRockAgainstScissors() throws Exception {
+        //given
+        final int playerInput = 1;
+        final int opponentInput = 3;
+
+        //when
+        gameService.checkingBattleResults(playerInput,opponentInput);
+
+
+        //then
+        assertThat(gameService.getOpponentScore()).isEqualTo(0);
+        assertThat(gameService.getPlayerScore()).isEqualTo(1);
+    }
+
+
 
     @Test
     public void opponentPicksWeaponBetweenOneAndThree() throws Exception {
