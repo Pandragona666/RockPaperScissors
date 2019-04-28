915e15b6577b2c51a355bfee796542d63a32e23d
Pandragona <asiuspospolitus@gmail.com>
2019-04-28 15:33:31 +0200

    added better describtions for ParameterizedTests

--- src/test/java/pl/kordas/portfolio/GameServiceTest.java
+++ src/test/java/pl/kordas/portfolio/GameServiceTest.java
@@ -19,11 +19,11 @@ public class GameServiceTest {
     //("1 - Rock ; 2 - Paper ; 3 - Scissors");
     @ParameterizedTest(name = "expecting \"{2}\" when given {0} and {1}")
     @CsvSource({
-            // given: 1 and 1, expected "0"
+            // given: 1 and 1 (Rock vs. Rock) , expected "0" for both
             "1, 1, 0, 0",
-            // given: 2 and 2, expected "0"
+            // given: 2 and 2 (Paper vs. Paper), expected "0" for both
             "2, 2, 0, 0",
-            // given: 3 and 3, expected "0"
+            // given: 3 and 3 (Scissors vs. Scissors), expected "0" for both
             "3, 3, 0, 0",
     })
     public void checkingBattleInCaseOfTheSameWeapons(int inputPlayer, int inputOpponent, int expectedPlayerScore, int expectedOpponentScore) throws Exception {
@@ -38,17 +38,17 @@ public class GameServiceTest {
     //("1 - Rock ; 2 - Paper ; 3 - Scissors");
     @ParameterizedTest(name = "expecting \"{2}\" when given {0} and {1}")
     @CsvSource({
-            // given: 1 and 2, expected "0"
+            // given: 1 and 2 (Rock vs. Paper), expected "0" for Player and "1" for Opponent
             "1, 2, 0, 1",
-            // given: 1 and 3, expected "1"
+            // given: 1 and 3, (Rock vs. Scissors), expected "1" for Player and "0" for Opponent
             "1, 3, 1, 0",
-            // given: 2 and 1, expected "1"
+            // given: 2 and 1, (Paper vs. Rock), expected "1" for Player and "0" for Opponent
             "2, 1, 1, 0",
-            // given: 2 and 3, expected "0"
+            // given: 2 and 3, (Paper vs. Scissors), expected "0" for Player and "1" for Opponent
             "2, 3, 0, 1",
-            // given: 3 and 1, expected "0"
+            // given: 3 and 1, (Scissors vs. Rock), expected "0" for Player and "1" for Opponent
             "3, 1, 0, 1",
-            // given: 3 and 2, expected "1"
+            // given: 3 and 2, (Scissors vs. Paper), expected "1" for Player and "0" for Opponent
             "3, 2, 1, 0",
     })
     public void checkingBattleInCaseOfDifferentWeapon(int inputPlayer, int inputOpponent, int expectedPlayerScore, int expectedOpponentScore){
